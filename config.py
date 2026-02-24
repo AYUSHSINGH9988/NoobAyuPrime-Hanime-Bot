9747c801d45afa79210da8edb5bff20b65c5b62f
AYUSHSINGH9988 <ayushsingh7337@gmail.com>
2026-02-24 14:05:49 +0530

    Delete config.py

--- config.py
+++
@@ -1,7 +0,0 @@
-import os
-
-class Config:
-    API_ID = int(os.environ.get("API_ID", "YOUR_API_ID_HERE"))
-    API_HASH = os.environ.get("API_HASH", "YOUR_API_HASH_HERE")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "YOUR_BOT_TOKEN_HERE")
-  
